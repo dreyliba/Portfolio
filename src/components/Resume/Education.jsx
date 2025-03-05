be6ca5e53d93b3787421c9bf3c84ba2265392028
dreyliba <libaandrey@gmail.com>
2025-03-05 15:33:10 +0800

    Resume Page Updated

--- src/components/Resume/Education.jsx
+++ src/components/Resume/Education.jsx
@@ -2,43 +2,40 @@ import { IoSchoolSharp } from "react-icons/io5";
 export default function Education() {
   const experiences = [
     {
-      year: "2023 - Present",
-      role: "MLG College of Learning, Inc.",
-      company: "Bachelor of Science in Information Technology",
-      description:
-        "Developing and maintaining modern web applications with backend and frontend integration.",
+      year: "2018 - 2022",
+      role: "College",
+      company: "MLG College of Learning, Inc.",
+      description: "Bachelor of Science in Information Technology",
     },
     {
-      year: "2021 - 2023",
-      role: "Hilongos High School",
-      company: "Personal Computer & Hardware Servicing",
-      description:
-        "Worked on responsive UI designs and interactive user experiences using React.",
+      year: "2016 - 2018",
+      role: "",
+      company: "Balocawehay National High School",
+      description: "General Academic Strand",
     },
-    {
-      year: "2019 - 2021",
-      role: "San Isidro Primary School",
-      company: "Handsome Corps. Inc.",
-      description:
-        "Created marketing materials, logos, and branding assets for various clients.",
-    },
+    // {
+    //   year: "2019 - 2021",
+    //   role: "Katipunan Primary School",
+    //   company: "Handsome Corps. Inc.",
+    //   description: "",
+    // },
   ];
 
   return (
     <section className="">
-      <h2 className="text-2xl font-bold mb-8 text-darkSecondary dark:text-lightSecondary text-end md:text-start">
+      <h2 className="mb-8 text-2xl font-bold text-darkSecondary dark:text-lightSecondary text-end md:text-start">
         Education
       </h2>
-      <div className="relative border-r-2 md:border-r-0 md:border-l-2 border-purple-500 mr-6 md:ml-6">
+      <div className="relative mr-6 border-purple-500 bor6der-r-2 md:border-r-0 md:border-l-2 md:ml-6">
         {experiences.map((exp, index) => (
           <div key={index} className="mb-10 mr-6 md:mr-0 md:ml-6">
             {/* Dot */}
-            <div className="absolute w-4 h-4 bg-purple-500 rounded-full -right-2 md:-left-2 border-2 border-white dark:border-darkPrimary"></div>
+            <div className="absolute w-4 h-4 bg-purple-500 border-2 border-white rounded-full -right-2 md:-left-2 dark:border-darkPrimary"></div>
             {/* Content */}
             <p className="text-sm text-gray-500 text-end md:text-start">
               {exp.year}
             </p>
-            <h3 className="text-xl font-semibold text-darkPrimary dark:text-lightPrimary flex items-center justify-end md:justify-start gap-2 ">
+            <h3 className="flex items-center justify-end gap-2 text-xl font-semibold text-darkPrimary dark:text-lightPrimary md:justify-start ">
               <span className="order-2 md:order-1">
                 <IoSchoolSharp className="text-purple-500 " />
               </span>
@@ -47,7 +44,7 @@ export default function Education() {
             <h4 className="text-sm font-medium text-gray-600 dark:text-gray-400 text-end md:text-start">
               {exp.company}
             </h4>
-            <p className="text-xs text-gray-600 dark:text-gray-400 mt-2 text-end md:text-start">
+            <p className="mt-2 text-xs text-gray-600 dark:text-gray-400 text-end md:text-start">
               {exp.description}
             </p>
           </div>
--- src/components/Resume/WorkExperience.jsx
+++ src/components/Resume/WorkExperience.jsx
@@ -3,47 +3,47 @@ import { FaBriefcase } from "react-icons/fa";
 export default function WorkExperience() {
   const experiences = [
     {
-      year: "2023 - Present",
+      year: "2025 - Present",
+      role: "Software Engineer",
+      company: "Accenture",
+      description:
+        "Design, develop, test, and maintain software for a variety of devices and systems",
+    },
+    {
+      year: "2022 - 2024",
       role: "Full-Stack Web Developer",
-      company: "AndrianTech Solutions",
+      company: "CreativeDevLabs",
       description:
         "Developing and maintaining modern web applications with backend and frontend integration.",
     },
     {
-      year: "2021 - 2023",
-      role: "Frontend Developer",
-      company: "Creative Agency",
-      description:
-        "Worked on responsive UI designs and interactive user experiences using React.",
-    },
-    {
       year: "2019 - 2021",
-      role: "Graphic Designer",
-      company: "Pixel Studio",
+      role: "IT Chat Support",
+      company: "MLG",
       description:
-        "Created marketing materials, logos, and branding assets for various clients.",
+        "Addressing students' queries related to both the Learning Management System (LMS) and the Portal.",
     },
   ];
 
   return (
     <section className="">
-      <h2 className="text-2xl font-bold mb-8 text-darkSecondary dark:text-lightSecondary">
+      <h2 className="mb-8 text-2xl font-bold text-darkSecondary dark:text-lightSecondary">
         Work Experience
       </h2>
-      <div className="relative border-l-2 border-purple-500 ml-6">
+      <div className="relative ml-6 border-l-2 border-purple-500">
         {experiences.map((exp, index) => (
           <div key={index} className="mb-10 ml-6">
             {/* Dot */}
-            <div className="absolute w-4 h-4 bg-purple-500 rounded-full -left-2 border-2 border-white dark:border-darkPrimary"></div>
+            <div className="absolute w-4 h-4 bg-purple-500 border-2 border-white rounded-full -left-2 dark:border-darkPrimary"></div>
             {/* Content */}
             <p className="text-sm text-gray-500">{exp.year}</p>
-            <h3 className="text-xl font-semibold text-darkPrimary dark:text-lightPrimary flex items-center gap-2">
+            <h3 className="flex items-center gap-2 text-xl font-semibold text-darkPrimary dark:text-lightPrimary">
               <FaBriefcase className="text-purple-500" /> {exp.role}
             </h3>
             <h4 className="text-sm font-medium text-gray-600 dark:text-gray-400">
               {exp.company}
             </h4>
-            <p className="text-xs text-gray-600 dark:text-gray-400 mt-2">
+            <p className="mt-2 text-xs text-gray-600 dark:text-gray-400">
               {exp.description}
             </p>
           </div>
